a2d1001338620effdba37a8fbdb78f77cdfb5f3d
Luisa Tutau <58300331+Lu31416@users.noreply.github.com>
2023-04-23 15:39:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function mostrarTexto1() {
+  document.getElementById("texto1").innerHTML = "Nace Maria Salomea Sklodowska em Varsóvia, Polônia, no dia 7 de novembro de 1867.";
+  }
+
+function mostrarTexto2() {
+  document.getElementById("texto2").innerHTML = "Marie Curie vai para Paris, onde começa a estudar física, matemática e química na Sorbonne.";
+    }
+function mostrarTexto3() {
+  document.getElementById("texto3").innerHTML = "Marie conhece Pierre Curie, que se tornaria seu marido e colaborador científico.";
+  }
+
+function mostrarTexto4() {
+  document.getElementById("texto4").innerHTML = "Marie Curie e Pierre Curie descobrem o elemento químico polônio.";
+    }
+
+function mostrarTexto5() {
+  document.getElementById("texto5").innerHTML = "arie Curie, Pierre Curie e Antoine Henri Becquerel recebem o Prêmio Nobel de Física pela descoberta da radioatividade";
+  }
+
+function mostrarTexto6() {
+  document.getElementById("texto6").innerHTML = "Pierre Curie morre em um acidente de carro em Paris.";
+    }
+function mostrarTexto7() {
+  document.getElementById("texto7").innerHTML = "Marie Curie recebe o Prêmio Nobel de Química pela descoberta do rádio e isolamento do elemento.";
+  }
+
+function mostrarTexto8() {
+  document.getElementById("texto8").innerHTML = "Marie Curie morre em Passy, Haute-Savoie, França, no dia 4 de julho de 1934, devido à exposição à radiação";
+    }
